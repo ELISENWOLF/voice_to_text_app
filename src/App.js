b5f546e46877285d1ea843d7ae0606230f011ca3
ELISENWOLF <aravindprakash2001@gmail.com>
2022-08-21 12:53:42 +0530

    voice-coverter

---
+++ src/App.js
@@ -0,0 +1,82 @@
+import React, { useState, useEffect } from "react";
+
+const SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+const mic = new SpeechRecognition();
+
+mic.continuous = true;
+mic.interimResults = true;
+mic.lang = 'en-Us';
+
+function App() {
+  const [isListening, setIsListening] = useState(false);
+  const [note, setNote] = useState(null);
+  const [saveNote, setSaveNote] = useState([]);
+
+  const handleListen= () => {
+    if (isListening) {
+      mic.start();
+      mic.onend = () => {
+        console.log("continue...");
+        mic.start();
+      }
+    } else {
+      mic.stop();
+      mic.onend = () => {
+        console.log("Stopped Mic on Click");
+      }
+    }
+    mic.onstart = () => {
+      console.log("Mic on");
+    };
+
+    mic.onresult = event => {
+      const transcript = Array.from(event.results)
+        .map((result) => result[0])
+        .map((result) => result.transcript)
+        .join('');
+        console.log(transcript);
+        setNote(transcript)
+        mic.onerror = event => {
+          console.log(event.error);
+        }
+    };
+  };
+
+  const handleSavenote = () => {
+    setSaveNote([...saveNote, note]);
+    setNote(''); 
+  }
+  
+  useEffect(() => {
+    handleListen();
+  }, [isListening])
+
+
+  return (
+    <>
+      <h1>Voice Note</h1>
+      <div className="container">
+        <div className="box">
+          <h2>Current Note</h2>
+          {isListening ? <span>Recording</span> : <span>Not Recording</span>}
+          <button onClick={handleSavenote} disabled={!note}>
+            Save
+          </button>
+          <button onClick={() => setIsListening(prevState => !prevState)}>
+            Start/Stop
+          </button>
+          <p>{note}</p>
+        </div>
+        <div className="box">
+          <h2>Notes</h2>
+          {saveNote.map(n => (
+            <p key={n}>{n}</p>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
