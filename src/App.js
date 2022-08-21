2e2aa0c63b27e945d2f7281f094d365cda79ee8f
ELISENWOLF <aravindprakash2001@gmail.com>
2022-08-21 13:00:08 +0530

    error-correction

--- src/App.js
+++ src/App.js
@@ -1,67 +1,67 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react'
+import './App.css'
 
 const SpeechRecognition =
-  window.SpeechRecognition || window.webkitSpeechRecognition;
-const mic = new SpeechRecognition();
+  window.SpeechRecognition || window.webkitSpeechRecognition
+const mic = new SpeechRecognition()
 
-mic.continuous = true;
-mic.interimResults = true;
-mic.lang = 'en-Us';
+mic.continuous = true
+mic.interimResults = true
+mic.lang = 'en-US'
 
 function App() {
-  const [isListening, setIsListening] = useState(false);
-  const [note, setNote] = useState(null);
-  const [saveNote, setSaveNote] = useState([]);
+  const [isListening, setIsListening] = useState(false)
+  const [note, setNote] = useState(null)
+  const [savedNotes, setSavedNotes] = useState([])
 
-  const handleListen= () => {
+  useEffect(() => {
+    handleListen()
+  }, [isListening])
+
+  const handleListen = () => {
     if (isListening) {
-      mic.start();
+      mic.start()
       mic.onend = () => {
-        console.log("continue...");
-        mic.start();
+        console.log('continue..')
+        mic.start()
       }
     } else {
-      mic.stop();
+      mic.stop()
       mic.onend = () => {
-        console.log("Stopped Mic on Click");
+        console.log('Stopped Mic on Click')
       }
     }
     mic.onstart = () => {
-      console.log("Mic on");
-    };
+      console.log('Mics on')
+    }
 
     mic.onresult = event => {
       const transcript = Array.from(event.results)
-        .map((result) => result[0])
-        .map((result) => result.transcript)
-        .join('');
-        console.log(transcript);
-        setNote(transcript)
-        mic.onerror = event => {
-          console.log(event.error);
-        }
-    };
-  };
-
-  const handleSavenote = () => {
-    setSaveNote([...saveNote, note]);
-    setNote(''); 
+        .map(result => result[0])
+        .map(result => result.transcript)
+        .join('')
+      console.log(transcript)
+      setNote(transcript)
+      mic.onerror = event => {
+        console.log(event.error)
+      }
+    }
   }
-  
-  useEffect(() => {
-    handleListen();
-  }, [isListening])
 
+  const handleSaveNote = () => {
+    setSavedNotes([...savedNotes, note])
+    setNote('')
+  }
 
   return (
     <>
-      <h1>Voice Note</h1>
+      <h1>Voice Notes</h1>
       <div className="container">
         <div className="box">
           <h2>Current Note</h2>
-          {isListening ? <span>Recording</span> : <span>Not Recording</span>}
-          <button onClick={handleSavenote} disabled={!note}>
-            Save
+          {isListening ? <span>🎙️</span> : <span>🛑🎙️</span>}
+          <button onClick={handleSaveNote} disabled={!note}>
+            Save Note
           </button>
           <button onClick={() => setIsListening(prevState => !prevState)}>
             Start/Stop
@@ -70,13 +70,13 @@ function App() {
         </div>
         <div className="box">
           <h2>Notes</h2>
-          {saveNote.map(n => (
+          {savedNotes.map(n => (
             <p key={n}>{n}</p>
           ))}
         </div>
       </div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
